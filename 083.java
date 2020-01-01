ed3865577fbc32bcaaecabe3ba34c555f1c893e3
feiqingjing <qingjing_02@126.com>
2020-01-01 17:22:40 +0800

    submit 083.

---
+++ 083.java
@@ -0,0 +1,39 @@
+//给定一个排序链表，删除所有重复的元素，使得每个元素只出现一次。
+//
+// 示例 1:
+//
+// 输入: 1->1->2
+//输出: 1->2
+//
+//
+// 示例 2:
+//
+// 输入: 1->1->2->3->3
+//输出: 1->2->3
+// Related Topics 链表
+
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+// 与26.删除排序数组中的重复项相似（链表版！）
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode node = head;
+        while (node != null && node.next != null) {
+            if (node.val == node.next.val)
+                node.next = node.next.next;
+            else
+                node = node.next;
+        }
+        return head;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
