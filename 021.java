663644a1a64a8a8579d92560139021336bb73a52
feiqingjing <qingjing_02@126.com>
2020-01-03 18:13:56 +0800

    submit 21 with 2 solutions.

---
+++ 021.java
@@ -0,0 +1,64 @@
+//将两个有序链表合并为一个新的有序链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+//
+// 示例：
+//
+// 输入：1->2->4, 1->3->4
+//输出：1->1->2->3->4->4
+//
+// Related Topics 链表
+
+
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+import java.util.LinkedList;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+// 思路：
+// 1. 递归，两个链表中头部元素较小者，用它剩余的元素与另一个链表merge
+// 2. (利用reference机制)创建新链表，只有一个头节点。
+//    两个指针遍历两链表，新链表节点指向遍历中值较小的节点
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+        if (l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists(l2.next, l1);
+            return l2;
+        }
+    }
+}
+
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+        ListNode result = new ListNode(0);  //a fake head where the head node is 0
+        ListNode cur = result;
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                cur.next = l1;
+                l1 = l1.next;
+            } else {
+                cur.next = l2;
+                l2 = l2.next;
+            }
+            cur = cur.next;
+        }
+        if (l1 != null) cur.next = l1;
+        if (l2 != null) cur.next = l2;
+        return result.next;  //Do not return fake head!
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
