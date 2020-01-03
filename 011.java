1e301c43bfd3775853545451dd840d15fe6c08dc
feiqingjing <qingjing_02@126.com>
2020-01-03 16:41:02 +0800

    submit 11 with 3 solutions.

--- 011.java
+++ 011.java
@@ -17,15 +17,49 @@
 
 
 //leetcode submit region begin(Prohibit modification and deletion)
+// 主流解法：双指针夹逼遍历所有可能的组合并计算面积
+// class Solution {
+//     public int maxArea(int[] height) {
+//         int maxArea = 0;
+//         for (int i = 0, j = height.length - 1; i < j; ) {
+//             maxArea = Math.max(maxArea, Math.min(height[i], height[j]) * (j - i));
+//             if (height[i] < height[j]) i++;
+//             else j--;
+//         }
+//         return maxArea;
+//     }
+// }
+// 效率改进版
 class Solution {
     public int maxArea(int[] height) {
         int maxArea = 0;
-        for(int i = 0, j = height.length - 1; i < j; ) {
+        for (int i = 0, j = height.length - 1; i < j; ) {
             maxArea = Math.max(maxArea, Math.min(height[i], height[j]) * (j - i));
-            if(height[i] < height[j]) i++;
-            else j--;
+            int k = 1;
+            if (height[i] < height[j]) {
+                while (height[i] > height[i+k]) k++;
+                i += k;
+//                while (i < j && height[i] > height[++i]);  // 此写法私以为逻辑有漏洞
+            }
+            else {
+                while (height[j] > height[j-k]) k++;
+                j -= k;
+//                while (i < j && height[j] > height[--j]);  // 此写法私以为逻辑有漏洞
+            }
         }
         return maxArea;
     }
 }
+// 写法改进版
+//class Solution {
+//    public int maxArea(int[] height) {
+//        int max = 0;
+//        for (int i = 0, j = height.length - 1; i < j; ) {
+//            int minHeight = height[i] < height[j] ? height[i++] : height[j--];
+//            int area = (j - i + 1) * minHeight;
+//            max = Math.max(max, area);
+//        }
+//        return max;
+//    }
+//}
 //leetcode submit region end(Prohibit modification and deletion)
