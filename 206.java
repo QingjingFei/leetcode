f8454ebaaaed4844bb0d0c292e71dc5e21cdd97b
feiqingjing <qingjing_02@126.com>
2020-01-07 15:47:56 +0800

    submit 206 with 2 solutions.

--- 206.java
+++ 206.java
@@ -11,6 +11,7 @@
 
 
 
+
 //leetcode submit region begin(Prohibit modification and deletion)
 /**
  * Definition for singly-linked list.
@@ -20,16 +21,28 @@
  *     ListNode(int x) { val = x; }
  * }
  */
+// 思路：
+// 1. 迭代，小心指针操作
+// 2. 递归，注意1）最后尾指针要为null；2）递归终止条件head.next==null返回head
+//class Solution {
+//    public ListNode reverseList(ListNode head) {
+//        ListNode prev = null;
+//        while (head != null) {
+//            ListNode next = head.next;
+//            head.next = prev;
+//            prev = head;
+//            head = next;
+//        }
+//        return prev;
+//    }
+//}
 class Solution {
     public ListNode reverseList(ListNode head) {
-        ListNode prev = null;
-        while (head != null) {
-            ListNode next = head.next;
-            head.next = prev;
-            prev = head;
-            head = next;
-        }
-        return prev;
+        if (head == null || head.next == null) return head;
+        ListNode p = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return p;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
