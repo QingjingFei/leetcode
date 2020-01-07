51b3da8cf6e8c088fbfb451f89ee16b356ea2ff0
feiqingjing <qingjing_02@126.com>
2020-01-07 16:09:03 +0800

    modify 58 with 2 solutions.

--- 058.java
+++ 058.java
@@ -14,9 +14,25 @@
 
 
 //leetcode submit region begin(Prohibit modification and deletion)
+// 思路：
+// 1. 直接调java字符串操作api
+// 2. 一个指针，String.charAt(i)遍历，注意java中""和''严格区分字符串与字符
+// 补：注意特例，调用String.trim()
 class Solution {
     public int lengthOfLastWord(String s) {
+//        return s.length() - s.lastIndexOf(" ") - 1;
         return s.trim().length()-s.trim().lastIndexOf(" ")-1;
     }
 }
+//class Solution {
+//    public int lengthOfLastWord(String s) {
+//        s = s.trim();
+//        int count = 0;
+//        for (int i = s.length() - 1; i >= 0; i--) {
+//            if (s.charAt(i) != ' ') count++;
+//            else break;
+//        }
+//        return count;
+//    }
+//}
 //leetcode submit region end(Prohibit modification and deletion)
