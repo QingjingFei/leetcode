5ffb77fe0b31689cb066b343328f5c5cf8229972
feiqingjing <qingjing_02@126.com>
2020-01-16 16:39:28 +0800

    submit 198 with 4 solutions.

--- 198.java
+++ 198.java
@@ -23,16 +23,60 @@
 
 
 //leetcode submit region begin(Prohibit modification and deletion)
+// 思路：
+// 1. 递归 —— Time Limit Exceeded
+// 2. 递归 + memo（从顶向下）
+// 3. 迭代 + memo（从底向上）
+// 4. 迭代 + 2变量记录（从底向上）
+//class Solution {
+//    public int rob(int[] nums) {
+//        return rob(nums, nums.length - 1);
+//    }
+//
+//    private int rob(int[] nums, int i) {
+//        if (i < 0) return 0;
+//        return Math.max(rob(nums, i - 2) + nums[i], rob(nums, i - 1));
+//    }
+//}
+//class Solution {
+//    int[] memo;
+//    public int rob(int[] nums) {
+//        memo = new int[nums.length + 1];
+//        Arrays.fill(memo, -1);
+//        return rob(nums, nums.length - 1);
+//    }
+//
+//    private int rob(int[] nums, int i) {
+//        if (i < 0) return 0;
+//        if (memo[i] >= 0) return memo[i];
+//        int res = Math.max(rob(nums, i -2) + nums[i], rob(nums, i - 1));
+//        memo[i] = res;
+//        return res;
+//    }
+//}
 class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
         if (n <= 1) return n == 0 ? 0 : nums[0];
         int[] memo = new int[n];
         memo[0] = nums[0];
-        memo[1] = Math.max(memo[0], memo[1]);
+        memo[1] = Math.max(nums[0], nums[1]);
         for (int i = 2; i < n; i++)
             memo[i] = Math.max(memo[i - 1], nums[i] + memo[i - 2]);
         return memo[n - 1];
     }
 }
+//class Solution {
+//    public int rob (int[] nums) {
+//        if (nums.length == 0) return 0;
+//        int prev1 = 0;
+//        int prev2 = 0;
+//        for (int num : nums) {
+//            int tmp = prev1;
+//            prev1 = Math.max(prev2 + num, prev1);
+//            prev2 = tmp;
+//        }
+//        return prev1;
+//    }
+//}
 //leetcode submit region end(Prohibit modification and deletion)
