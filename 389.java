53f7b5d042fc19efdbcfecef053589d20a48a120
feiqingjing <qingjing_02@126.com>
2020-02-07 20:41:08 +0800

    submit 389.

---
+++ 389.java
@@ -0,0 +1,51 @@
+//给定两个字符串 s 和 t，它们只包含小写字母。
+//
+// 字符串 t 由字符串 s 随机重排，然后在随机位置添加一个字母。
+//
+// 请找出在 t 中被添加的字母。
+//
+//
+//
+// 示例:
+//
+// 输入：
+//s = "abcd"
+//t = "abcde"
+//
+//输出：
+//e
+//
+//解释：
+//'e' 是那个被添加的字母。
+//
+// Related Topics 位运算 哈希表
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+// 思路：
+// 异或 -> 相同位都会得0，仅留下相异位
+class Solution {
+    public char findTheDifference(String s, String t) {
+        char c = 0;
+        for (int i = 0; i < s.length(); i++) {
+            c ^= s.charAt(i);
+        }
+        for (int i = 0; i < t.length(); i++) {
+            c ^= t.charAt(i);
+        }
+        return c;
+    }
+}
+// 改进版
+class Solution {
+    public char findTheDifference(String s, String t) {
+        int n = t.length();
+        char c = t.charAt(n - 1);
+        for (int i = 0; i < n - 1; i++) {
+            c ^= s.charAt(i);
+            c ^= t.charAt(i);
+        }
+        return c;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
