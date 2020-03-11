3126fcd89546580f6d88e8695a15f69d6dab011e
feiqingjing <qingjing_02@126.com>
2020-03-11 19:24:04 +0800

    resubmit 141.

--- 141.java
+++ 141.java
@@ -54,12 +54,12 @@
 public class Solution {
     public boolean hasCycle(ListNode head) {
         if (head == null) return false;
-        ListNode walker = head;
-        ListNode runner = head;
-        while(runner != null && runner.next != null) {
-            walker = walker.next;
-            runner = runner.next.next;
-            if (walker == runner) return true;
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) return true;
         }
         return false;
     }
