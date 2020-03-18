e900f40ba44ab416d9f50266944d1d6237fa10c6
feiqingjing <qingjing_02@126.com>
2020-03-18 16:55:34 +0800

    resubmit 104.

--- 104.java
+++ 104.java
@@ -48,56 +48,57 @@ class Solution {
         return Math.max(leftDepth, rightDepth) + 1;
     }
 }
-//class Solution {
-//    // add/remove/get是抛出异常的
-//    // offer/poll/peek是返回特定值的
-//    public int maxDepth(TreeNode root) {
-//        if (root == null) {
-//            return 0;
-//        }
-//        LinkedList<TreeNode> queue = new LinkedList<>();
-//        queue.addLast(root);
-//        int maxDepth = 0;
-//        while (!queue.isEmpty()) {
-//            maxDepth++;
-//            int levelSize = queue.size();  // 一定要在此处用队列大小固定住for循环的次数！！！
-//            for (int i = 0; i < levelSize; i++) {
-//                TreeNode node = queue.removeFirst();
-//                if (node.left != null) {
-//                    queue.addLast(node.left);
-//                }
-//                if (node.right != null) {
-//                    queue.addLast(node.right);
-//                }
-//            }
-//        }
-//        return maxDepth;
-//    }
-//}
-//class Solution {
-//    // 此法leetcode不能通过！cannot find symbol Pair
-//    public int maxDepth(TreeNode root) {
-//        if (root == null) {
-//            return 0;
-//        }
-//        LinkedList<Pair<TreeNode, Integer>> stack = new LinkedList<>();
-//        stack.push(new Pair<>(root, 1));
-//        int maxDepth = 0;
-//        while (!stack.isEmpth()) {
-//            Pair<TreeNode, Integer> pair = stack.pop();
-//            TreeNode node = pair.first;
-//            maxDepth = Math.max(maxDepth, pair.second);
-//            int curDepth = pair.second;
-//            // 注意！！栈是先进后出，所以后面先进right再进left；
-//            // 可以保证出栈为DFS先序遍历为中左右的自然顺序
-//            if (node.right != null) {
-//                stack.push(new Pair<>(node.right, curDepth + 1));
-//            }
-//            if (node.left != null) {
-//                stack.push(new Pair<>(node.left, curDepth + 1));
-//            }
-//        }
-//        return maxDepth;
-//    }
-//}
+class Solution {
+    // add/remove/get是抛出异常的
+    // offer/poll/peek是返回特定值的
+    // poll = removeFirst，offer = addLast
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int maxDepth = 0;
+        while (!queue.isEmpty()) {
+            maxDepth++;
+            int levelSize = queue.size();  // 一定要在此处用队列大小固定住for循环的次数！！！
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode node = queue.poll();
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+        }
+        return maxDepth;
+    }
+}
+class Solution {
+    // 此法leetcode不能通过！cannot find symbol Pair
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        LinkedList<Pair<TreeNode, Integer>> stack = new LinkedList<>();
+        stack.push(new Pair<>(root, 1));
+        int maxDepth = 0;
+        while (!stack.isEmpth()) {
+            Pair<TreeNode, Integer> pair = stack.pop();
+            TreeNode node = pair.first;
+            maxDepth = Math.max(maxDepth, pair.second);
+            int curDepth = pair.second;
+            // 注意！！栈是先进后出，所以后面先进right再进left；
+            // 可以保证出栈为DFS先序遍历为中左右的自然顺序
+            if (node.right != null) {
+                stack.push(new Pair<>(node.right, curDepth + 1));
+            }
+            if (node.left != null) {
+                stack.push(new Pair<>(node.left, curDepth + 1));
+            }
+        }
+        return maxDepth;
+    }
+}
 //leetcode submit region end(Prohibit modification and deletion)
